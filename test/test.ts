f37a28e0eb5c8750858895f5af2779d92437c1e8
FilipHarald <filip.harald@gmail.com>
2022-07-19 11:37:29 +0200

    restructuring tests and adding tests for performance

--- test/test.ts
+++ test/test.ts
@@ -2,15 +2,27 @@ import {expect} from 'chai';
 import {calculate} from '../src/volatility-calculator';
 
 describe('Volatility calculation', () => {
-  describe('volatility metric', () => {
-    const orders = [
+    const fewOrders = [
       [200, 19960, 20000, 19960],
       [132, 19990, 20110, 20050],
       [66, 20050, 20060, 20055],
       [1, 20000, 20010, 20005],
     ];
+  describe('correctness', () => {
     it('should be correct', () => {
-      expect(calculate(orders)).equal(0.36147860256782666);
+      expect(calculate(fewOrders)).equal(0.36147860256782666);
+    });
+  });
+  describe('speed', () => {
+    it('should be fast enough', () => {
+      const start = process.hrtime();
+      calculate(fewOrders);
+      const [secondsDiff, nanosecondsDiff] = process.hrtime(start);
+      expect(secondsDiff).equal(0);
+      expect(nanosecondsDiff).below(1_000_000);
+    });
+    it('should be blazingly fast', () => {
+      // TODO
     });
   });
 });
