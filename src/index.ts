d32b2360b7660c8caa3d0167a69ac5f5a9ed8506
FilipHarald <filip.harald@gmail.com>
2022-07-19 22:16:22 +0200

    starting with setting up Grafana and Influx

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,14 @@ const client = new Client();
 
 client.on("l2update", l2update => {
 
-//  const l = {
-//    ...l2update,
-//    asks: l2update.asks.slice(0, 10),
-//    bids: l2update.bids.slice(0, 10)
-//  };
-//  console.log(l);
-
   const start = process.hrtime();
   const volatility = calculator.update(l2update);
-  const [secondsDiff, nanosecondsDiff] = process.hrtime(start);
+  const [,nanosecondsDiff] = process.hrtime(start);
   console.log(`
 timestamp:      ${Date.now()}
 volatility:     ${volatility}
 execution time: ${(nanosecondsDiff) / 1_000_000}`);
+
 });
 
 console.log('starting...');
