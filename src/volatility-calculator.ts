bb0db7fc5ba12cebfd723643a6f1939bf6b2c158
FilipHarald <filip.harald@gmail.com>
2022-07-19 11:03:27 +0200

    first working metric

--- src/volatility-calculator.ts
+++ src/volatility-calculator.ts
@@ -1,4 +1,15 @@
-const calculate = () => 1
+const TIMESTAMP_INDEX = 0;
+const MIDPRICE_INDEX = 3;
+
+const calculate = (orders: number[][]) => {
+  return orders.slice(0, orders.length-1).reduce((acc, currOrder, index) => {
+    return acc + (
+      Math.abs(currOrder[MIDPRICE_INDEX] - orders[index + 1][MIDPRICE_INDEX])
+      / currOrder[MIDPRICE_INDEX]
+      / (currOrder[TIMESTAMP_INDEX] - orders[index + 1][TIMESTAMP_INDEX])
+    );
+  }, 0) / (orders.length - 1) * 10_000;
+};
 
 export {
   calculate
--- test/test.ts
+++ test/test.ts
@@ -2,9 +2,15 @@ import {expect} from 'chai';
 import {calculate} from '../src/volatility-calculator';
 
 describe('Volatility calculation', () => {
-  describe('correctness', () => {
+  describe('volatility metric', () => {
+    const orders = [
+      [200, 19960, 20000, 19960],
+      [132, 19990, 20110, 20050],
+      [66, 20050, 20060, 20055],
+      [1, 20000, 20010, 20005],
+    ];
     it('should be correct', () => {
-      expect(calculate(1)).equal(1);
+      expect(calculate(orders)).equal(0.36147860256782666);
     });
   });
 });
